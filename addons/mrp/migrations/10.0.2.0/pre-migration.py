531d7b670618828095b197474c76c20f2f27b68d
mreficent <miquel.raich@eficent.com>
2017-09-13 18:30:54 +0200

    [ADD] Migration scrips

---
+++ addons/mrp/migrations/10.0.2.0/pre-migration.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent <http://www.eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+
+_column_copies = {
+    'mrp_production': [
+        ('state', None, None)
+    ],
+    'stock_move': [
+        ('consumed_for', None, None),
+    ],
+}
+
+_column_renames = {
+    'mrp_bom': [
+        ('product_uom', 'product_uom_id'),
+    ],
+}
+
+_table_renames = [
+    ('mrp_production_workcenter_line', 'mrp_workorder'),
+]
+
+_xmlid_renames = [
+    ('base.menu_mrp_config', 'mrp.menu_mrp_config'),
+    ('base.menu_mrp_root', 'mrp.menu_mrp_root'),
+]
+
+
+def prepopulate_fields(cr):
+    cr.execute(
+        """
+        ALTER TABLE mrp_production
+        ADD COLUMN picking_type_id INTEGER
+        """
+    )
+
+
+@openupgrade.migrate(use_env=False)
+def migrate(cr, version):
+    openupgrade.copy_columns(cr, _column_copies)
+    openupgrade.rename_columns(cr, _column_renames)
+    openupgrade.rename_tables(cr, _table_renames)
+    prepopulate_fields(cr)
+    openupgrade.rename_xmlids(cr, _xmlid_renames)
