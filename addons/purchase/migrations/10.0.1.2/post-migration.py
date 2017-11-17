4ba3c312cab90a6d28a9a4a04de3e817939010a6
Stefan Rijnhart <stefan@opener.am>
2017-11-17 21:02:31 +0100

    [FIX] Store date_planned of purchase orders

--- addons/purchase/migrations/10.0.1.2/post-migration.py
+++ addons/purchase/migrations/10.0.1.2/post-migration.py
@@ -5,8 +5,22 @@
 from openupgradelib import openupgrade
 
 
-@openupgrade.migrate(use_env=False)
-def migrate(cr, version):
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
     openupgrade.load_data(
-        cr, 'purchase', 'migrations/10.0.1.2/noupdate_changes.xml',
+        env.cr, 'purchase', 'migrations/10.0.1.2/noupdate_changes.xml',
     )
+
+    # date_planned was not stored in 9.0 but we may have the dates from 8.0
+    # lying around somewhere. For other records recompute.
+    if openupgrade.column_exists(
+            env.cr, 'purchase_order', 'minimum_planned_date'):
+        env.cr.execute(
+            """UPDATE purchase_order
+            SET date_planned = minimum_planned_date
+            WHERE date_planned IS NULL""")
+    to_recompute = env['purchase.order'].search(
+        [('date_planned', '=', False)])
+    env.add_todo(
+        env['purchase.order']._fields['amount_total'], to_recompute)
+    env['purchase.order'].recompute()
